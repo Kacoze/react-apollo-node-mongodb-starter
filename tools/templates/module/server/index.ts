27faadd15beb7003c5ac47bbef20daabe13500d4
Kamil Kozieradzki <Kamil.Kozieradzki@gmail.com>
2018-11-04 12:32:33 +0100

    cli fix

--- tools/templates/module/server/index.ts
+++ tools/templates/module/server/index.ts
@@ -1,4 +1,4 @@
-import $Module$ from './sql';
+import $Module$ from './db';
 import schema from './schema.graphql';
 import createResolvers from './resolvers';
 import ServerModule from '../ServerModule';
